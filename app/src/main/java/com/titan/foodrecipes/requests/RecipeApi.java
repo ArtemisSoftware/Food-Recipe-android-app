7817d3f26a44ea64ba802b343aa104613560c44b
turim09 <gustavo.isaac@gmail.com>
2019-11-04 22:34:29 +0000

    Retrofit Interface Methods

--- app/src/main/java/com/titan/foodrecipes/requests/RecipeApi.java
+++ app/src/main/java/com/titan/foodrecipes/requests/RecipeApi.java
@@ -1,4 +1,19 @@
 package com.titan.foodrecipes.requests;
 
+import com.titan.foodrecipes.requests.responses.RecipeResponse;
+import com.titan.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    // SEARCH
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(@Query("key") String key, @Query("q") String query, @Query("page") String page);
+
+    // GET SPECIFIC RECIPE
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(@Query("key") String key, @Query("rId") String recipe_id);
 }
