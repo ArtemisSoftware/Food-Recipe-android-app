20ceca24d7466493662cdddc245612fa1b9377a2
turim09 <gustavo.isaac@gmail.com>
2019-11-04 22:00:52 +0000

    API Key

---
+++ app/src/main/java/com/titan/foodrecipes/util/Constants.java
@@ -0,0 +1,7 @@
+package com.titan.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.food2fork.com";
+    public static final String API_KEY = "bfe5b89b70c37ff3a0d7731b8eac07e3";
+}
