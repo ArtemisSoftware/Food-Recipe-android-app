3477547758f045a467e2b3e3fd0d132030eb17c1
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-18 22:28:09 +0000

    New Api for the app

--- app/src/main/java/com/titan/foodrecipes/util/Constants.java
+++ app/src/main/java/com/titan/foodrecipes/util/Constants.java
@@ -2,12 +2,17 @@ package com.titan.foodrecipes.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://www.food2fork.com";
-    public static final String API_KEY = "4a4476650d4e1b3923fbd1ae5cbd3ec6";//"d3ab033003c2e546e131f5b45402e3e9";//"bfe5b89b70c37ff3a0d7731b8eac07e3";
+    //food2fork does not work anymore
+    //public static final String BASE_URL = "https://www.food2fork.com";
+    // public static final String API_KEY = "4a4476650d4e1b3923fbd1ae5cbd3ec6";//"d3ab033003c2e546e131f5b45402e3e9";//"bfe5b89b70c37ff3a0d7731b8eac07e3";
+
+    //Alternativa para o food2fork
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    public static final String API_KEY = "";
 
     public static final int CONNECTION_TIMEOUT = 10; // 10 seconds
-    public static final int READ_TIMEOUT = 2; // 2 seconds
-    public static final int WRITE_TIMEOUT = 2; // 2 seconds
+    public static final int READ_TIMEOUT = 20; // 2 seconds
+    public static final int WRITE_TIMEOUT = 20; // 2 seconds
 
     public static final int NETWORK_TIMEOUT = 3000;
 
